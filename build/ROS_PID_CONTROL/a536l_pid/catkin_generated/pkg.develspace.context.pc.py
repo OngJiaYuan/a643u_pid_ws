133cbc60d738386d5cb5445132f4d4c51b0891a1
Ongjy <jiayuan.ong@kabam.ai>
2022-10-17 15:30:57 +0800

    initial commit

--- build/ROS_PID_CONTROL/a536l_pid/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/jy/a643u_pid_ws/src/ROS_PID_CONTROL/a536l_pid/include".split(';') if "/home/jy/a643u_pid_ws/src/ROS_PID_CONTROL/a536l_pid/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "roscpp;std_msgs;sensor_msgs;nav_msgs;geometry_msgs;gazebo_msgs".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "a536l_pid"
-PROJECT_SPACE_DIR = "/home/jy/a643u_pid_ws/devel"
-PROJECT_VERSION = "0.0.0"
